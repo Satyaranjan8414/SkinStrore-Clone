71994c159c3c3f56b018ebc7a09185e889f13558
satya M <satya2maity@gmail.com>
2022-12-18 02:27:34 +0530

    added new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./Components/Pages/Footer";
+import Navbar from "./Components/Pages/Navbar";
+import Shop from "./Components/ProductsPage/ProductPage";
+import AllRoutes from "./Components/Roouters/AllRoutes";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Roouters/AllRoutes.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../Auth/Login';
+import SignUp from '../Auth/SignUp';
+
+import BathBeauty from '../Pages/All_Products_Pages/Bath&Beauty';
+import Blog from '../Pages/All_Products_Pages/Blog';
+import Brands from '../Pages/All_Products_Pages/Brands';
+import BuildingRoute from '../Pages/All_Products_Pages/BuildingRoute';
+import Checkout from '../Pages/All_Products_Pages/Checkout';
+import Fragrence from '../Pages/All_Products_Pages/Fragrence';
+
+import Holiday from '../Pages/All_Products_Pages/Holiday';
+import Makeup from '../Pages/All_Products_Pages/Makeup';
+import HairPages from "../Pages/All_Products_Pages/HairPages"
+import NewsTrending from '../Pages/All_Products_Pages/NewsTrending';
+import Sale from '../Pages/All_Products_Pages/Sale';
+import SelfCare from '../Pages/All_Products_Pages/Self-Care';
+
+import SkinPage from '../Pages/All_Products_Pages/SkinPage';
+import Tools from '../Pages/All_Products_Pages/Tools';
+import Cart from "../Pages/Cart";
+import Home from '../Pages/Home';
+import Shop from '../ProductsPage/ProductPage';
+import PrivateRoute from './PrivateRoute';
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/skin' element={<Shop />} />
+        <Route path='/bathbeauty' element={<BathBeauty />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/brands' element={<Brands />} />
+        <Route path='/building' element={<BuildingRoute />} />
+        <Route path='/fragrence' element={<Fragrence />} />
+        <Route path='/hair' element={<HairPages />} />
+        <Route path='/holiday' element={<Holiday />} />
+        <Route path='/makeup' element={<Makeup />} />
+        <Route path='/news' element={<NewsTrending />} />
+        <Route path='/sale' element={<Sale />} />
+        <Route path='/selfcare' element={<SelfCare />} />
+        <Route path='/tools' element={<Tools />} />
+        <Route path='/checkout' element={<PrivateRoute><Checkout /></PrivateRoute>} />
+        <Route path='/skincare' element={<SkinPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default AllRoutes;
